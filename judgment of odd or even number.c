44fc23d76141891b994c6833ca95e9cc85522597
EAXID <76682657+EAXID@users.noreply.github.com>
2021-02-05 21:02:32 +0800

    Add files via upload

---
+++ judgment of odd or even number.c
@@ -0,0 +1,20 @@
+// WL2020 2031053231 WXD
+// judgment of odd or even number
+
+#include<stdio.h>
+
+int main(void)
+{
+	int integer,odd,even;
+	scanf("d",&integer);
+	if ((integer%2)==0)
+		{
+		puts("even");
+		}
+	
+	else
+	    {
+		puts("odd");
+		}
+		
+}
