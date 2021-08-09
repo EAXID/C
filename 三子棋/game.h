522bca4dbff38add0966ce4ba68e0be4eace25fd
EAXID <76682657+EAXID@users.noreply.github.com>
2021-08-09 22:02:41 +0800

    Add files via upload

---
+++ 三子棋/game.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+#define ROW 3
+#define COL 3
+
+void InitBoard(char board[ROW][COL], int row, int col);
+void DisplayBoard(char board[ROW][COL], int row, int col);
+void PlayerMove(char board[ROW][COL], int row, int col);
+void ComputerMove(char board[ROW][COL], int row, int col);
+char IsWin(char board[ROW][COL], int row, int col);
+int IsFull(char board[ROW][COL], int row, int col);
